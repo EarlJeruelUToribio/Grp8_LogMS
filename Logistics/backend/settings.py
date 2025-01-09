dd7803291bf88d968e2231559c947b51dfd846e8
Fredzxcs <johnfredericktsabondo@iskolarngbayan.pup.edu.ph>
2025-01-09 14:24:33 +0800

    commit

--- Logistics/backend/settings.py
+++ Logistics/backend/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'logistic',
-        # 'USER':'postgres',
-        # 'PASSWORD': 'jeffskieS21',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'logistic',
+        'USER':'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
 
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME':BASE_DIR / 'db.sqlite3',
     }
 }
 
