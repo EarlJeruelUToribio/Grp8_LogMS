b256555868ed03a207d9e573f8504ee63a844088
RichardCabacu <132378703+RichardCabacu@users.noreply.github.com>
2025-01-10 21:50:19 +0800

    update
    update

--- Logistics/backend/settings.py
+++ Logistics/backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER':'postgres',
-        'PASSWORD': 'hayaanmuna01',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
