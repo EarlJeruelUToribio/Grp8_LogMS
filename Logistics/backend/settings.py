4d9aa4001c1b81c95253453bb16800ac48b28985
EarlJeruelUToribio <earljeruelutoribio@iskolarngbayan.pup.edu.ph>
2025-01-20 22:18:17 +0800

    commit

--- Logistics/backend/settings.py
+++ Logistics/backend/settings.py
@@ -15,6 +15,8 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import dj_database_url
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
