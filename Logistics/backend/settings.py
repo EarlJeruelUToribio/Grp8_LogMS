554df165293e30c6f3765b4a37f3fb670b371283
Chad <richardecabacungan@iskolarngabayan.pup.edu.ph>
2025-01-25 03:49:43 +0800

    search bar design
    search bar design

--- Logistics/backend/settings.py
+++ Logistics/backend/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'logistic',
         'USER':'postgres',
-        'PASSWORD': 'hayaanmuna01',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
