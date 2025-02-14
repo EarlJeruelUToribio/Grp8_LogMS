b26cf44d2d5c41bf1247b784bf89e0299a8ecab4
Chad <richardecabacungan@iskolarngabayan.pup.edu.ph>
2025-02-15 00:29:42 +0800

    dashboard
    dashboard

--- Logistics/backend/settings.py
+++ Logistics/backend/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-ss7f)bgp8f=91nqt6b2m&z*3tx*kq%p4$b+w1%t6kbu0w38hl!
 DEBUG = True
 
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.241.44','logistics-5mci.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.1.53','logistics-5mci.onrender.com']
 
 
 
@@ -68,7 +68,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8002',
-    'http://192.168.241.151:8002',  # Reservation project's address
+    'http://192.168.1.84:8002',  # Reservation project's address
 ]
 
 CORS_ALLOW_HEADERS = [
