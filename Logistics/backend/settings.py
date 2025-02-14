2f9ade7a92a16b6ec991c39bf9f7804f57a546cf
EarlJeruelUToribio <earljeruelutoribio@iskolarngbayan.pup.edu.ph>
2025-02-14 21:30:53 +0800

    render setup

--- Logistics/backend/settings.py
+++ Logistics/backend/settings.py
@@ -107,14 +107,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'logistic',
-        'USER':'postgres',
-        'PASSWORD': 'hayaanmuna01',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
