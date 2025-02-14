875b3a87af96d301123a13b3e0761bff16b47c0e
Chad <richardecabacungan@iskolarngabayan.pup.edu.ph>
2025-02-15 01:02:36 +0800

    dashboard new UI
    dashboard new UI

--- Logistics/backend/settings.py
+++ Logistics/backend/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'logistic',
         'USER':'postgres',
-        'PASSWORD': 'hayaanmuna01',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432'
     }
